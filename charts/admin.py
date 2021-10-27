9ed0db062e6bee868c436ee9a3de672a91a09ec7
julienrobert17 <59126582+julienrobert17@users.noreply.github.com>
2021-10-27 15:13:37 +0200

    sec

--- charts/admin.py
+++ charts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from carts.models import Cart
 
+admin.site.register(Cart)
 # Register your models here.
--- charts/models.py
+++ charts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from common.models import Base
 
-# Create your models here.
+
+class Cart(Base):
+    name = models.CharField(max_length=80, null=True)
+    owner = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="carts"
+    )
+
+    def __str__(self):
+        return self.name
